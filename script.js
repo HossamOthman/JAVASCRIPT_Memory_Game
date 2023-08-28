4525f04bad5f99b99921812a2af8ccbf2b3c5152
Hossam Othman <rowheadrex@gmail.com>
2023-08-29 00:36:25 +0200

    Matching Cards

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+cards.forEach((card) => card.addEventListener("click", flipCard));
 
+function flipCard() {
+  this.classList.toggle("flip");
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
 
-
-function flipCard(){
-    this.classList.toggle('flip');
+    // do they match ?
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        }, 1500);
+    }
+  }
 }
